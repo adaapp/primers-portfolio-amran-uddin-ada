4ef23997aba28a994e48f651527e9fdd1ce28fc0
amran-uddin-ada <89026853+amran-uddin-ada@users.noreply.github.com>
2021-08-19 15:10:43 +0000

    primer 8 two threads running concurrently

--- include/thursday.h
+++ include/thursday.h
@@ -3,7 +3,10 @@
 using namespace std;
 
 void sleepDuration(int seconds);
-
+void timer1();
+void timer2();
+int t1Duration = 5;
+int t2Duration = 10;
 void sleepTimer(void) {
   cout << "sleepTimer: Calling sleep function with thread Id: " <<std::this_thread::get_id() <<"\n";
   sleepDuration(10);
@@ -12,12 +15,25 @@ void sleepTimer(void) {
 
 
 void joinDetachThreads(void) {
-	std::cout << " - joinDetachThreads: not yet implemented\n\n";
+  cout << "Main Thread: " <<std::this_thread::get_id() <<" started\n";
+  std::thread thread1(&timer1);
+  std::thread thread2(&timer2);
+  thread1.join();
+  thread2.join();
+  cout << "Main Thread: " <<std::this_thread::get_id() <<" ended\n";
 }
 
 void sleepDuration(int seconds){
-  cout << "sleepDuration: Putting to sleep Thread: " <<std::this_thread::get_id() <<" for " << seconds << " seconds\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(seconds*1000));
-  cout << "sleepDuration: Function finished\n";
+  std::this_thread::sleep_for(std::chrono::milliseconds(seconds*1000));
+}
+void timer1(){
+  cout << "Thread 1: " <<std::this_thread::get_id() <<" started\n";
+  sleepDuration(t1Duration);
+  cout << "Thread 1: " <<std::this_thread::get_id() <<" ended\n";
 
+}
+void timer2(){
+  cout << "Thread 2: " <<std::this_thread::get_id() <<" started\n";
+  sleepDuration(t2Duration);
+  cout << "Thread 2: " <<std::this_thread::get_id() <<" ended\n";
 }
